51201c7cfbbd24381f3401bacc6d7f25b98f6382
alhammad108 <115951421+alhammad108@users.noreply.github.com>
2023-03-02 17:14:38 +0300

    Update my_testCase.java

--- src/my_testCase.java
+++ src/my_testCase.java
@@ -1,62 +1,103 @@
 import static org.testng.Assert.assertEquals;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
-public class my_testCase extends paramiter{
+public class xyz_Bank_test extends prametre_xyz_Bank {
 @Test()
-public void chick_the_Title() {
-	String Actual_title=driver.getTitle();
-	assertEquals(Actual_title, expected_title);
-	mysoftassert.assertAll();
-}
-@Test()
-public void chick_the_imeges_drower() {
-	List<WebElement> my_imeges=driver.findElements(By.className("img-fluid"));
-	boolean image_check1 = my_imeges.get(0).getAttribute("src")==my_imeges.get(1).getAttribute("src");
-	boolean image_check2 = my_imeges.get(1).getAttribute("src")==my_imeges.get(2).getAttribute("src");
-	boolean image_check3 = my_imeges.get(2).getAttribute("src")==my_imeges.get(1).getAttribute("src");
-	mysoftassert.assertEquals(image_check1, false,"image one with image two");
-	mysoftassert.assertEquals(image_check2, false,"image two with image three ");
-	mysoftassert.assertEquals(image_check3, false,"image three with image one");
-	mysoftassert.assertAll();
-}
+public void Log_in() {
+	String Actual_tittle=driver.getTitle();
+	assertEquals(Actual_tittle, expected_title);
 	
-	@Test
-	public void validate_Contact() throws InterruptedException {
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-		String the_name_Of_emils []= {"a1@gmail.com","a2@gmail.com","a3@gmail.com","a4@gmail.com"};
-Random check_Email=new Random();
-int num_Rand =  check_Email.nextInt(4);
-		driver.findElement(By.xpath("//*[@id=\"navbarExample\"]/ul/li[2]/a")).click();
-		
-		driver.findElement(By.xpath("//*[@id=\"recipient-email\"]")).sendKeys(the_name_Of_emils[num_Rand]);
-		Thread.sleep(3000);
-String attribute_Of_email = driver.findElement(By.xpath("//*[@id=\"recipient-email\"]")).getAttribute("value");
-String regex = "^(.+)@(.+)$";
-
-
-Pattern pattern = Pattern.compile(regex);
-
-
-Matcher matcher = pattern.matcher(the_name_Of_emils[num_Rand]);
-System.out.println(the_name_Of_emils[num_Rand]+"is valid "+matcher.matches());
-
-
-boolean my_check_Of_themaches = matcher.matches();
-mysoftassert.assertEquals(my_check_Of_themaches,true);
-
 mysoftassert.assertAll();
+}
+@Test(priority= 1,groups="customer")
+public void LoginAscustomer() {
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+			
+	driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[1]/button")).click();
+}
+	@Test(priority = 1, groups = "maneger")
+	public void LoginAsmaneger() {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/button")).click();
 	}
+
+	@Test(priority = 2, groups = "maneger")
+	public void Addcustomer() throws InterruptedException {
+		int userID = (int) (Math.random() * 100);
+		StringBuilder userName = new StringBuilder();
+		userName.append(userID);
+		String UserIdAsstring = userName.toString();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/button[1]")).click();
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input")).sendKeys("Mr");
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[2]/input"))
+				.sendKeys("User" + UserIdAsstring);
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[3]/input"))
+				.sendKeys(UserIdAsstring);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/button")).click();
+		Thread.sleep(2000);
+		String Mytextalert = driver.switchTo().alert().getText();
+
+		boolean chickalert = Mytextalert.contains("Customer added successfully");
+		assertEquals(chickalert, true);
+
+		driver.switchTo().alert().accept();
+
+		driver.navigate().to("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager/list");
+
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/form/div/div/input"))
+				.sendKeys("User" + UserIdAsstring);
+
+		mysoftassert.assertAll();
+	}
+@Test()
+public void Open_customer() throws InterruptedException {
+	String bigamount="1000";
+	String smallamouont="600";
+int num1=	Integer.parseInt(smallamouont);
+	int num2 = Integer.parseInt(bigamount);
 	
+	
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+	driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[1]/button")).click();
+	Thread.sleep(2000);
+	
+	Select Myselect = new Select(driver.findElement(By.id("userSelect")));
+
+Myselect.selectByVisibleText("Harry Potter");
+Thread.sleep(2000);
+driver.findElement(By.xpath("/html/body/div/div/div[2]/div/form/button")).click();
+Thread.sleep(2000);
+
+driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[3]/button[2]")).click();
+driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[4]/div/form/div/input")).sendKeys(bigamount);
+
+driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[4]/div/form/button")).click();
+Thread.sleep(2000);
+driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[3]/button[3]")).click();
+Thread.sleep(2000); 
+driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[4]/div/form/div/input")).sendKeys(smallamouont);
+Thread.sleep(2000);
+driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[4]/div/form/button")).click();
+
+String balance= driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/strong[2]")).getText();
+Thread.sleep(2000);
+
+assertEquals(balance,num2-num1);
+mysoftassert.assertAll();
+
 }
 
+
+	
+}
 
